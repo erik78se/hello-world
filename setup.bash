bcd489b9c64efb08346bc4c069d0079ec1a4c11b
Erik Lönroth <erik.lonroth@gmail.com>
2017-10-03 09:32:50 +0200

    added bash source for charm dev

---
+++ setup.bash
@@ -0,0 +1,8 @@
+#
+# bashrc
+#
+export CHARM_ROOT=$HOME/git/juju/charms
+export LAYER_PATH=$CHARM_ROOT/layers
+export INTERFACE_PATH=$CHARM_ROOT/interfaces
+export JUJU_REPOSITORY=$CHARM_ROOT/repo
+
